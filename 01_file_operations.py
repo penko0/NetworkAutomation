8898e4c7bd46efc5039152d2cd6738a94e7d1c05
penko0 <p__ivanov@inbox.ru>
2024-12-19 11:52:28 +0200

    asd

--- 01_file_operations.py
+++ 01_file_operations.py
@@ -1,6 +1,12 @@
 import os
 print(f"Current dir is {os.getcwd()}")
-os.chdir('Docker')
+os.chdir('../Docker')
 print(f"Dir changed to {os.getcwd()}")
 print(os.listdir())
-print(len(os.listdir()))
+print(f"The number of files and folders is {len(os.listdir())}")
+print(os.system('ls -larth'))
+
+files = (os.listdir())
+files.sort() #Sorts the file names 
+for file in files:
+    print(file)
