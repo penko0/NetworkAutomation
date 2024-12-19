f9272d42f49b1330208a7b6b059d3ef8b625b8e3
penko0 <p__ivanov@inbox.ru>
2024-12-19 12:02:43 +0200

    dasda

--- 01_file_operations.py
+++ 01_file_operations.py
@@ -1,12 +1,18 @@
 import os
-print(f"Current dir is {os.getcwd()}")
-os.chdir('../Docker')
-print(f"Dir changed to {os.getcwd()}")
-print(os.listdir())
-print(f"The number of files and folders is {len(os.listdir())}")
-print(os.system('ls -larth'))
 
-files = (os.listdir())
-files.sort() #Sorts the file names 
-for file in files:
-    print(file)
+# print(f"Current dir is {os.getcwd()}")
+# os.chdir('../Docker')
+# print(f"Dir changed to {os.getcwd()}")
+# print(os.listdir())
+# print(f"The number of files and folders is {len(os.listdir())}")
+# print(os.system('ls -larth')) #Execute a command on our operating system
+
+# files = (os.listdir())
+# files.sort() #Sorts the file names 
+# for file in files:
+#     print(file)
+
+#How to open/print a file
+
+file1 = open('config1.txt', 'r')
+print(dir(file1))
