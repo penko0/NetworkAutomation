3a7ff22e9021abbed7007077486a542af47b065d
penko0 <p__ivanov@inbox.ru>
2024-12-19 12:41:07 +0200

    finished text file operations chapter

--- 01_file_operations.py
+++ 01_file_operations.py
@@ -15,4 +15,34 @@ import os
 #How to open/print a file
 
 file1 = open('config1.txt', 'r')
-print(dir(file1))
+#print(dir(file1)) #With dir we print the methods available for that file. In order to read the file we use "read" method.
+
+#print(file1.read()) #Read method reads the whole file
+#print(file1.readlines()) #Prints the content in a list format.
+
+# commands = file1.readlines()
+# for command in commands:
+#     print(command.rstrip('\n'))
+file1.close()  #Always use .close() method because without it , the file will stay open in the memory
+################
+'''with open method'''
+# with open('config1.txt') as file1:
+#     print(file1.readlines())
+
+################
+'''writing content to a file'''
+
+with open('config2.txt', 'w') as file2:
+    file2.write("testdata\n")
+
+#Append data without overwriting the file
+
+with open('config2.txt', 'a') as file2:
+    file2.write("testdata12")
+
+#Create new file and add some text to it:
+with open('new_file.txt', 'w') as dest_file:
+    dest_file.write('some text')
+
+#How to remove a file
+os.remove('new_file.txt')
