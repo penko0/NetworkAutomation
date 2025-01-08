36e92efdf2420dcfd061cd6e652ff8b48e5ea1ab
ipe586 <ivanov.penko@scania.com>
2025-01-08 18:51:26 +0200

    new files

--- 24_Nornir/01_Nornir_basic_task.py
+++ 24_Nornir/01_Nornir_basic_task.py
@@ -2,11 +2,18 @@
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
 from datetime import datetime
-# nr = InitNornir(config_file="config.yaml") #Config file in YAML format
+from pprint import pprint
+
+nr = InitNornir(config_file="config.yaml") #Config file in YAML format
 start_time = datetime.now()
-nr = InitNornir() #Here nornir looks for default config file hosts.yaml
+# nr = InitNornir() #Here nornir looks for default config file hosts.yaml
 results = nr.run(task=napalm_get, getters=['facts', 'interfaces'])
 
 print_result(results)
 end_time = datetime.now()
 print(end_time - start_time)
+
+#Print a dictionary results
+print(results['rtr1'].result)
+#To filter the results, use filter. Here we will see only interface configs
+pprint(results['rtr1'].result['interfaces'])
