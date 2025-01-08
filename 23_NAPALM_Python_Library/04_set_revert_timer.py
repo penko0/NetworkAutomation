eb08a9fdc470549dbdf8f40b7b7bb8c54085a3b9
penko0 <p__ivanov@inbox.ru>
2025-01-08 14:54:22 +0200

    new files

--- 23_NAPALM_Python_Library/04_set_revert_timer.py
+++ 23_NAPALM_Python_Library/04_set_revert_timer.py
@@ -6,21 +6,14 @@ device = driver(hostname='192.168.1.23', username='admin', password='admin')
 device.open()
 print("Connected successfully")
 
-device.load_merge_candidate(config='interface loopback1003\n ip address 2.1.1.1 255.255.255.0')
-
-''' We can specify configuration file also , instead to write the commands here'''
-# device.load_merge_candidate(filename='config.txt')
+device.load_replace_candidate(filename='bkp.txt')
 
 print(device.compare_config())
 if len(device.compare_config()) > 0:
     choice = input("\nWould you like to commit these changes? [yN]: ")
     if choice == 'y':
         print("Committing ....")
-        device.commit_config()
-        choice = input("\nWould you like to Rollback to previous config? [yN]: ")
-        if choice == 'y':
-            print("Rolling back to previous config ...")
-            device.rollback()
+        device.commit_config(revert_in=60)
     else:
         print("Discarding ...")
         device.discard_config()
