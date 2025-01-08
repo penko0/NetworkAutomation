ee2593c9871b04bcd2fd42d822695b96ade2a78a
ipe586 <ivanov.penko@scania.com>
2025-01-08 18:35:07 +0200

    new files

--- 24_Nornir/01_Nornir_basic_task.py
+++ 24_Nornir/01_Nornir_basic_task.py
@@ -1,3 +1,12 @@
-from nornir import InitNornir
+﻿from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
+from datetime import datetime
+# nr = InitNornir(config_file="config.yaml") #Config file in YAML format
+start_time = datetime.now()
+nr = InitNornir() #Here nornir looks for default config file hosts.yaml
+results = nr.run(task=napalm_get, getters=['facts', 'interfaces'])
+
+print_result(results)
+end_time = datetime.now()
+print(end_time - start_time)
