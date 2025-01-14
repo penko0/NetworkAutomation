0e5ef0e53a867765804ba40eecbd074192bb85e7
penko0 <p__ivanov@inbox.ru>
2025-01-14 18:19:47 +0200

    edited file

--- 29_PyATS_Framework/02_mock_device_learn.py
+++ 29_PyATS_Framework/02_mock_device_learn.py
@@ -1,10 +1,7 @@
 from pyats.topology import loader
 
-
-
 ''' We use this 'mock' configuration when we do not have real devices but want to perform some tests. We have to download mock.zip from Cisco. '''
-
-
+#Seems ospf learn feature does not work on NX-OS as expected
 tb = loader.load('mock.yaml')
 dev = tb.devices["nx-osv-1"]
 dev.connect()
