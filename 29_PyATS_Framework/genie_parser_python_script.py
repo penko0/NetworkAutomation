d1159d2419e719fe58fb9c804c60bbd3dda60c58
penko0 <p__ivanov@inbox.ru>
2025-01-14 16:25:12 +0200

    new file

--- 29_PyATS_Framework/genie_parser_python_script.py
+++ 29_PyATS_Framework/genie_parser_python_script.py
@@ -1,6 +1,34 @@
 from genie.testbed import load
+from pprint import pprint
+from tabulate import tabulate
 tb = load('yaml/my_testbed.yaml')
 dev = tb.devices["CSR3"]
-dev.connect()
+
+dev.connect(log_stdout=False) # This will not print stdout to our terminal
+
 show_version_parsed = dev.parse("show version")
-print(show_version_parsed['version']['version_short'])
+show_ip_int_br_parsed = dev.parse("show ip interface brief")
+
+''' Creates a dictionary with interface data'''
+int_parsed = show_ip_int_br_parsed['interface']
+
+
+
+print(show_version_parsed, "\n")
+print(show_ip_int_br_parsed, "\n")
+
+# print(tabulate(show_ip_int_br_parsed)) #Just for tests , what will be printed on the screen
+
+''' Prints given interface from the dictionary that we created earlier'''
+# print(show_ip_int_br_parsed['interface']['GigabitEthernet2'])
+# print(show_version_parsed['version']['version_short'])
+
+# pprint(dir(dev.api)) # Print all the methods that we can use
+
+print(int_parsed)
+for int in int_parsed:
+    print(int)
+
+
+for int in int_parsed:
+    print(f"NAME: {int}, IP: {int_parsed[int]['ip_address']}")
